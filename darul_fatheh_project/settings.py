548adf677fe4bf6943cfe4a10f793988e3e50965
Abhijith P <abhijithp@Abhijiths-MacBook-Air.local>
2026-01-17 15:43:47 +0530

    changed settings in allowed hosts

--- darul_fatheh_project/settings.py
+++ darul_fatheh_project/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-for-dev'
 
 DEBUG = 'True'
 
-ALLOWED_HOSTS = ['darul-fatheh-2.onrender.com','*']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
